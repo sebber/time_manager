1bba1b4198b36d18c35d76a93970ba077ac3647c
Sebastian Bergman <basse@grafikfel.org>
2020-06-27 12:03:02 +0200

    add routing

--- assets/js/admin/Root.js
+++ assets/js/admin/Root.js
@@ -1,7 +1,33 @@
 import * as React from 'react';
+import {
+  BrowserRouter,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+import Home from './screens/Home';
+import Events from './screens/Events';
 
 export default function Root() {
-    return (
-        <h1>Hej världen!</h1>
-    )
+  return (
+    <BrowserRouter basename="/admin">
+      <div>
+        <nav>
+          <Link to="/">Home</Link>
+          <Link to="/events">Events</Link>
+        </nav>
+        <div>
+          <Switch>
+            <Route path="/events">
+              <Events />
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </div>
+      </div>
+    </BrowserRouter>
+  )
 };
